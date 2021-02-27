a2f7adeb4347a9745ec7fb2f18c70fa363060cec
xinnywinne <x.wang.tju2010@gmail.com>
2021-02-27 14:59:00 -0800

    intialize folder

---
+++ src/main/java/com/baibai/rush/RushApplication.java
@@ -0,0 +1,13 @@
+package com.baibai.rush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RushApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baibai/rush/RushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baibai.rush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RushApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
