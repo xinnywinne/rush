41b2bcf92d761fc516d3dbe81bfc3eaa43d8794a
Joe Wang <zhouqiang.wang@hotmail.com>
2021-02-27 17:40:10 -0800

    add po, mappers files

---
+++ src/main/java/com/baibai/rush/db/mappers/OrderMapper.java
@@ -0,0 +1,17 @@
+package com.baibai.rush.db.mappers;
+
+import com.baibai.rush.db.po.Order;
+
+public interface OrderMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Order record);
+
+    int insertSelective(Order record);
+
+    Order selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(Order record);
+
+    int updateByPrimaryKey(Order record);
+}
