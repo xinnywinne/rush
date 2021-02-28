7c76ce732e4cc204aeceb8a00253bc4c6112a191
xinnywinne <x.wang.tju2010@gmail.com>
2021-02-27 16:54:42 -0800

    application update

--- src/main/java/com/baibai/rush/RushApplication.java
+++ src/main/java/com/baibai/rush/RushApplication.java
@@ -1,9 +1,13 @@
 package com.baibai.rush;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@MapperScan("com.baibai.rush.db.mappers")
+@ComponentScan(basePackages = {"com.baibai"})
 public class RushApplication {
 
 	public static void main(String[] args) {
