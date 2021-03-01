a5e4bb3141bfb8e97f3d5ba968c6eb9309a0ff77
Joe Wang <zhouqiang.wang@hotmail.com>
2021-03-01 00:54:48 -0800

    Update RushActivityMapper.java

--- src/main/java/com/baibai/rush/db/mappers/RushActivityMapper.java
+++ src/main/java/com/baibai/rush/db/mappers/RushActivityMapper.java
@@ -1,6 +1,7 @@
 package com.baibai.rush.db.mappers;
 
 import com.baibai.rush.db.po.RushActivity;
+import java.util.List;
 
 public interface RushActivityMapper {
     int deleteByPrimaryKey(Long id);
@@ -14,4 +15,6 @@ public interface RushActivityMapper {
     int updateByPrimaryKeySelective(RushActivity record);
 
     int updateByPrimaryKey(RushActivity record);
+
+    List<RushActivity> queryRushActivitysByStatus(int activityStatus);
 }
